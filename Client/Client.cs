fd0061f722437abf0f347be34eb8792eebb7f129
Kevin van Dommele <kevinvd@live.nl>
2022-05-16 22:21:40 +0200

    Add project files.

---
+++ Client/Client.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class Client
+    {
+        private string _serverName;
+        public Common.PipeStream Stream { get; set; }
+        public NamedPipeClientStream ClientStream { get; set; }
+
+        public Client(string serverName)
+        {
+            _serverName = serverName;
+            ClientStream = new NamedPipeClientStream(".", serverName, PipeDirection.InOut, PipeOptions.None, System.Security.Principal.TokenImpersonationLevel.Impersonation);
+            Stream = new Common.PipeStream(ClientStream);
+        }
+
+        public void Connect()
+        {
+            ClientStream.Connect();
+        }
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class Server
+    {
+        private string _serverName;
+        public Common.PipeStream Stream { get; set; }
+        public NamedPipeServerStream ServerStream { get; set; }
+
+        public Server(string serverName)
+        {
+            _serverName = serverName;
+            ServerStream = new NamedPipeServerStream(serverName, PipeDirection.InOut, 1);
+            Stream = new Common.PipeStream(ServerStream);
+        }
+
+        public void WaitForConnection()
+        {
+            ServerStream.WaitForConnection();
+        }
+    }
+}
